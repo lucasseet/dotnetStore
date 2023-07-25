212e341a7fdadf498e892c87fe0ce6d29bfea13b
lucasseet <79414028+lucasseet@users.noreply.github.com>
2023-07-25 21:36:38 +0800

    UI with react

--- API/Program.cs
+++ API/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<StoreContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,7 +24,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
+app.UseCors(opt => {
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
 
 app.UseAuthorization();
 
