b1decb3d895dc6924515369aefa787f77605bc18
lucasseet <79414028+lucasseet@users.noreply.github.com>
2023-07-28 21:46:59 +0800

    error handling

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -6,7 +6,9 @@ import {
 } from "@mui/material";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import Header from "./Header";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -26,6 +28,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
